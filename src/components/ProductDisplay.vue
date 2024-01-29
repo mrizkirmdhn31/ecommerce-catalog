ec216482a3a6a2c62504acb584b91a5a8c2d59e7
Muhammad Rizki Ramadhan <54527854+mrizkirmdhn31@users.noreply.github.com>
2024-01-28 16:23:13 -0800

    Add files via upload

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,55 +1,61 @@
 <template>
     <div class="container">
-        <div v-if="isShow" class="card">
-            <div class="product-container"></div>
-            <div class="detail">
-                <div class="detail-top"></div>
-                <div class="detail-bottom"></div>
+        <div v-if="isLoading" class="card">
+            <div class="product-container">
+                <div class="default-thumbnail"></div>
+                <div class="default-details">
+                    <div class="details-top"></div>
+                    <div class="details-bottom"></div>
+                </div>
             </div>
         </div>
-        <div v-else class="container" :class="!productShow ? 'bg-shadow-gray' : product.data.category === 'men\'s clothing' ? 'bg-pale-aqua' : 'bg-ping'">
+        <div v-else class="container" :class="!isProduct ? 'bg-gray' : product.data.category === 'men\'s clothing' ? 'bg-blue' : 'bg-pink'">
             <div class="overlay">
-                <img src="" alt="">
+                <img src="../assets/bg-shape.svg">
             </div>
             <div class="card">
-                <div v-if="!productShow" class="product-unavailable">
+                <div v-if="!isProduct" class="product-unavailable-container">
                     <div class="overlay">
-                        <img src="" alt="">
+                        <img src="../assets/bg-sad.svg">
                     </div>
-                    <div class="detail-product">
+                    <div class="product-details">
                         <p>This product is unavailable to show</p>
-                        <div class="animation">
-                            <button type="button" @click="theProduct()" :class="product.data.category === 'men\'s clothing' ? 'border-dark-sapphire' : 'border-byzantium font-byzantium'" class="button-next" >Next Product</button>
+                        <div class="button-action">
+                            <button type="button" @click="getProduct()" class="button-next">Next Product</button>
                         </div>
                     </div>
                 </div>
                 <div v-else class="product-container">
-                    <div class="thumbnail">
+                    <div class="product-thumbnail">
                         <img :src="product.data.image">
                     </div>
-                    <div class="detail-product">
+                    <div class="product-details">
                         <div class="top">
-                            <h2 :class="product.data.category === 'men\'s clothing' ? 'font-dark-sapphire' : 'font-byzantium'" class="title">{{ product.data.title }}</h2>
+                            <h3 :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="title">{{ product.data.title }}</h3>
                             <div class="sub-title">
-                                <div class="rate">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
+                                <span>{{ product.data.category }}</span>
+                                <div class="rating">
+                                    <span>{{ product.data.rating.rate }}/5</span>
+                                    <div class="rating">
+                                        <!-- <span v-for="(star, index) in stars" :key="index"></span> -->
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                        <span :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="circle"></span>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="description">
+                                <p>{{ product.data.description }}</p>
+                            </div>
                         </div>
-                        <div class="description">
-                            <p>{{ product.data.description }}</p>
-                        </div>
-                    </div>
-                    <div class="bottom">
-                        <span :class="product.data.category === 'men\'s clothing' ? 'font-dark-sapphire' : 'font-byzantium'" class="price">${{ product.data.price }}</span>
-                        <div class="animation">
-                            <button type="button" :class="product.data.category === 'men\'s clothing' ? 'bg-dark-sapphire' : 'bg-byzantium'" class="animation-buy">Buy Now</button>
-                            <button type="button" :class="product.data.category === 'men\'s clothing' ? 'border-dark-sapphire font-dark-sapphire' : 'border- byzantium font-byzantium'" class="animation-next">Next Product</button>
+                        <div class="bottom">
+                            <span :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="price">${{ product.data.price }}</span>
+                            <div class="button-action">
+                                <button type="button" :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="button-buy">Buy Now</button>
+                                <button type="button" @click="getProduct()" :class="product.data.category === 'men\'s clothing' ? 'border-navy font-navy' : 'border-magenta font-magenta'" class="button-next">Next Product</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -61,43 +67,57 @@
 <script>
 export default {
     name: 'ProductDisplay',
-    data(){
-        return{
-            product:{},
-            index:0,
-            isShow: false,
-            productShow: false
+    data() {
+        return {
+            product: {},
+            isLoading: false,
+            index: 0,
+            isProduct: false,
+            // isRating: false
         }
     },
+    methods: {
+        async getAPI() {
+            const response = await fetch(`https://fakestoreapi.com/products/${this.index}`);
+            const result = await response.json();
+            return result;
+        },
+        async getProduct() {
+            this.isLoading = true;
 
-methods: {
-    async callAPI(){
-        const response = await fetch(`https://fakestoreapi.com/products/${this.index}`);
-        const result = await response.json();
-        return result;
+            if (this.index !== 20) {
+                this.index++
+            } else {
+                this.index = 1;
+            }
+
+            let data = await this.getAPI()
+            if (data.category === "men's clothing" || data.category === "women's clothing") {
+                this.product = { data }
+                this.isProduct = true;
+            } else {
+                this.isProduct = false;
+            }
+
+            this.isLoading = false;
+        },
+        // async getRating(){
+            // this.isRating=true;
+            // let data = await this.getAPI()
+    //     }
+    // }
+            // stars(rating){
+            //     const fullCircle = Math.floor(data);
+            //     const halfCircle = data % 1 >= 0.5 ? 1 : 0;
+            //     const emtpyCircle = 5 - fullCircle - halfCircle;
+            //     return [
+            //         ...Array(fullCircle).fill({filled: true}),
+            //         ...Array(halfCircle).fill({filled: true, half:true}),
+            //         ...Array(emtpyCircle).fill({filled: false})
+            //     ]
     },
-    async theProduct() {
-        this.isShow = true;
-
-        if (this.index !== 20){
-            this.index++
-        } else {
-            this.index = 1
-        }
-
-        let data = await this.callAPI()
-        if (data.category === "men's clothing" || data.category === "women's clothing") {
-            this.product = {data}
-            this.productShow = true;
-        } else {
-            this.productShow = false;
-        }
-
-        this.isShow = false;
-    },
-},
     mounted() {
-        this.theProduct();
+        this.getProduct();
     },
 }
 </script>
